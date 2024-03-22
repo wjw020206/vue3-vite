6154a0611ee869ed898b1573be05ea2382bd9d7a
CodePencil <jiawei.wang2021@gmail.com>
2024-03-22 13:29:30 +0800

    build: 引入了Vue新版开发者工具

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -2,9 +2,10 @@ import type { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import UnoCSS from 'unocss/vite';
 import unplugin from './unplugin';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 /** 设置Vite插件 */
 export function setupVitePlugins() {
-  const plugins: PluginOption = [vue(), ...unplugin(), UnoCSS()];
+  const plugins: PluginOption = [vue(), VueDevTools(), ...unplugin(), UnoCSS()];
   return plugins;
 }
