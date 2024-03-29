2e16c4ac7fbc221ded520f4a7d0902840e2ded95
CodePencil <jiawei.wang2021@gmail.com>
2024-03-29 17:37:53 +0800

    refactor: 关闭了eslint对TypeScript类型的校验

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
   plugins: ['@typescript-eslint', 'vue'],
   rules: {
     'vue/multi-word-component-names': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-undef': 'off'
   }
 };
